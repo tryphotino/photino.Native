bac01b96a7953df71d7c4ac5706fdb444fc68f92
Vasco Sousa <myemailvasco@gmail.com>
2021-08-20 15:20:38 +0100

    Attemp to fake bundle for macOs removed

--- Photino.Native/Photino.Mac.AppDelegate.mm
+++ Photino.Native/Photino.Mac.AppDelegate.mm
@@ -2,40 +2,6 @@
 #import "Photino.Mac.AppDelegate.h"
 #import <objc/runtime.h>
 
-NSString * const PhotinoAppBundleID = @"com.photino.net";
-
-@implementation NSBundle (FakeBundleIdentifier)
-
-- (NSString *)__bundleIdentifier;
-{
-    return PhotinoAppBundleID;
-}
-
-@end
-
-static BOOL InstallFakeBundleIdentifierHook()
-{
-  Class nsBundleClass = objc_getClass("NSBundle");
-  if (nsBundleClass) {
-    method_exchangeImplementations(class_getInstanceMethod(nsBundleClass, @selector(bundleIdentifier)),
-                                   class_getInstanceMethod(nsBundleClass, @selector(__bundleIdentifier)));
-    return YES;
-  }
-  return NO;
-}
-
-@implementation NSUserDefaults (SubscriptAndUnescape)
-- (id)objectForKeyedSubscript:(id)key;
-{
-  id obj = [self objectForKey:key];
-  if ([obj isKindOfClass:[NSString class]] && [(NSString *)obj hasPrefix:@"\\"]) {
-    obj = [(NSString *)obj substringFromIndex:1];
-  }
-  return obj;
-}
-@end
-
-
 @implementation AppDelegate : NSObject
 - (id)init {
     if (self = [super init]) {
@@ -49,13 +15,6 @@ static BOOL InstallFakeBundleIdentifierHook()
     [window makeKeyAndOrderFront:nil];
     [NSApp activateIgnoringOtherApps:YES];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    
-    @autoreleasepool {
-        if (InstallFakeBundleIdentifierHook()) {
-            NSLog(@"%@", defaults[@"sender"]);
-        }
-    }
-
     //NSLog(@"applicationDidFinishLaunching fired!");
     //NSAlert *alert = [[[NSAlert alloc] init] autorelease];
     //[alert setMessageText:@"Hi there."];
