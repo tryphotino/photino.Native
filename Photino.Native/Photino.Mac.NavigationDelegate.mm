9cf27e37273603c6e583c54e735c0ae8b7817598
Philipp Bauer <hello@philippbauer.org>
2024-01-04 13:09:02 -0600

    Add completion handler for default (false)
    The app crashed because no completion handler was called when the IgnoreCertificationErrors option was not set or set to false.

--- Photino.Native/Photino.Mac.NavigationDelegate.mm
+++ Photino.Native/Photino.Mac.NavigationDelegate.mm
@@ -4,18 +4,22 @@
 @implementation NavigationDelegate : NSObject
 
     - (void)webView:(WKWebView *)webView 
-    didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
-    completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
-        bool ignoreCertificateErrorsEnabled = false;
-        photino->GetIgnoreCertificateErrorsEnabled(&ignoreCertificateErrorsEnabled);
-        if(ignoreCertificateErrorsEnabled)
-        {
-            SecTrustRef serverTrust = challenge.protectionSpace.serverTrust;
-            CFDataRef exceptions = SecTrustCopyExceptions(serverTrust);
-            CFRelease(exceptions);
-            completionHandler(NSURLSessionAuthChallengeUseCredential,[NSURLCredential credentialForTrust:serverTrust]);
+        didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+        completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler {
+            bool ignoreCertificateErrorsEnabled = false;
+            photino->GetIgnoreCertificateErrorsEnabled(&ignoreCertificateErrorsEnabled);
+            if(ignoreCertificateErrorsEnabled)
+            {
+                SecTrustRef serverTrust = challenge.protectionSpace.serverTrust;
+                CFDataRef exceptions = SecTrustCopyExceptions(serverTrust);
+                CFRelease(exceptions);
+                completionHandler(NSURLSessionAuthChallengeUseCredential, [NSURLCredential credentialForTrust:serverTrust]);
+            }
+            else
+            {
+                completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+            }
         }
-    }
 
 @end
 #endif
